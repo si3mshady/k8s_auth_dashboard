04c592530defc16d77d897f0c5ed9f2016695df2
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-10-03 21:37:27 -0500

    updated

--- server/server.js
+++ server/server.js
@@ -1,27 +1,63 @@
 const  https = require('https');
 const axios = require('axios')
 const fs = require('fs')
+const  express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const app=express();
+const port = 9000;
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
 
-const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImtmY1VSTHJ5MFB0Wmdsc25lNG0xaUtlcEJ0RmhGUXQwRWJNQ2dXamN5WUEifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNjY0ODQzMDg0LCJpYXQiOjE2NjQ4Mzk0ODQsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJkZWZhdWx0Iiwic2VydmljZWFjY291bnQiOnsibmFtZSI6ImRlZmF1bHQiLCJ1aWQiOiJhNTk0YmEyNi0wYmJmLTQ4OTItOWMxNy1hNzFlYzNhMGE0ZTEifX0sIm5iZiI6MTY2NDgzOTQ4NCwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6ZGVmYXVsdCJ9.WUYJ6EOBPnR8xGwmlaYJknMWbhLq-OfvU4iMoOXFvfyI9vFip_lPbQ8kCmTMigITL1141qCIn_J99romHJdV6e3BEglLzLDYDYO22FlkohtaP9aRZTe8hA_iqQDef23o25IPqH8-dLOeL1daN9y1ZfGTkTG0M6mSxulVt-FtoOWTSpVjNkOE_du_JWDVj9YtBwjfstxZm6Nrb9Sl_yJMdL45AzoUkT8RERAWZSYyYiKh0QjfVULwiY61Z9nPdu822sbqO9GccO7ANQE74-YCgLAVxEuKEnfih8Km-gEaLZv4p1dbXlLesPQezgdLJFp3t8Lj-ZK0folUTrm4hgqZQA'
-const header = {Authorization : `Bearer ${token}`}
+// const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImtmY1VSTHJ5MFB0Wmdsc25lNG0xaUtlcEJ0RmhGUXQwRWJNQ2dXamN5WUEifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNjY0ODUwNjY5LCJpYXQiOjE2NjQ4NDcwNjksImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJkZWZhdWx0Iiwic2VydmljZWFjY291bnQiOnsibmFtZSI6ImRlZmF1bHQiLCJ1aWQiOiJhNTk0YmEyNi0wYmJmLTQ4OTItOWMxNy1hNzFlYzNhMGE0ZTEifX0sIm5iZiI6MTY2NDg0NzA2OSwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6ZGVmYXVsdCJ9.LhveN5Hu2KT2VnqXf3OZPKqToEvnXAWcWmNJX-8P2MQLD3d9-xoBBZpC5LSUYDOb7HRT-gL7PgIVfaCND8s8htshtlE_iqoRgpG6p5MgJqrpGpHz3PLd1d0vfOu8XsI0WffnHzo1QJ0litvkOOPiDT5aAoZ3IcJpeSdUiOZxJLnyyKQ4fH--2-HWJJTNSdG_pjSvsrE1mdnsUXdQssgakbzlUYEcLsM02FpTEyVF-bKK0VUNh1Dd_f9-53VCTdSTQlvCGlovuWrR8GISaOCo5ugv3zHtONlIUD_qDgB5Qrpa8v1V59RyPEccD6hTggL15LH_B2Bmxoj5y5kgWiOJLg'
+// const header = {Authorization : `Bearer ${token}`}
 
 
-const sendRequest = async () => {
+app.post('/token', async (req,res) => {
+    
+    let token
+
+    // console.log('hello')
+
+    token = req.body.token
+    endpoint = req.body.endpoint
+
+    // console.log(token)
+    // console.log(endpoint)
+
+    data =  await sendRequest(token,endpoint)
+    // console.log(data)
+    return res.status(202).send(data)
+
+// }
+
+}
+)
+
+
+const sendRequest = async (token,endpoint='') => {
+
+    // console.log(res.data)
+
+    // token = res.data
+    const header = {Authorization : `Bearer ${token}`}
 
     const httpsAgent = new https.Agent( {
   
-      rejectUnauthorized: false
-  
-     
-    
-    })
-    // kubectl config view | grep http
+        rejectUnauthorized: false
 
+  
+    })
+  
+// 'https://127.0.0.1:54852/apis'
     try {
-        result = await axios.get('https://127.0.0.1:54852/apis', { httpsAgent:  httpsAgent, headers: header });
-    console.log(result.data)
+       const result = await axios.get(endpoint, { httpsAgent:  httpsAgent, headers: header });
+    // console.log(result.data)
+    return result.data
+
     } catch (error) {
         console.log(error)
     }
@@ -29,16 +65,8 @@ const sendRequest = async () => {
 
 
   
-  }
-
-  sendRequest()
+  } 
 
 
-
-//   $ curl https://127.0.0.1:54852/apis/apps/v1/ --header "Authorization: Bearer $JWT_TOKEN_DEFAULT_DEFAULT"
-// {
-//   "kind": "APIResourceList",
-//   "apiVersion": "v1",
-//   "groupVersion": "apps/v1",
-//   "resources": [...]
-// }
+//   sendRequest()
+  app.listen(port, () => console.log(`Listening on port : ${port}`))
